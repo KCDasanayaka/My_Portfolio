76624d44abc062df57c67dc71dc91745333068b1
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-09-05 14:24:41 +0530

    new updated

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect } from 'react';
+import './home.css';
+import github from '../assets/github.Pro.png';
+import behance from '../assets/behance.Pro.png';
+import linkedin from '../assets/linkedin.Pro.png';
+import Logo from '../assets/Logo.png';
+
+function Home() {
+  useEffect(() => {
+    consoleText(['UI UX Engineer', 'Full-Stack Dev'], 'text', ['#2AD87F']);
+  }, []);
+
+  function consoleText(words, id, colors) {
+    if (colors === undefined) colors = ['#fff'];
+    let visible = true;
+    const con = document.getElementById('console');
+    let letterCount = 1;
+    let x = 1;
+    let waiting = false;
+    const target = document.getElementById(id);
+    target.setAttribute('style', 'color:' + colors[0]);
+
+    setInterval(() => {
+      if (letterCount === 0 && !waiting) {
+        waiting = true;
+        target.innerHTML = words[0].substring(0, letterCount);
+        setTimeout(() => {
+          const usedColor = colors.shift();
+          colors.push(usedColor);
+          const usedWord = words.shift();
+          words.push(usedWord);
+          x = 1;
+          target.setAttribute('style', 'color:' + colors[0]);
+          letterCount += x;
+          waiting = false;
+        }, 1000);
+      } else if (letterCount === words[0].length + 1 && !waiting) {
+        waiting = true;
+        setTimeout(() => {
+          x = -1;
+          letterCount += x;
+          waiting = false;
+        }, 1000);
+      } else if (!waiting) {
+        target.innerHTML = words[0].substring(0, letterCount);
+        letterCount += x;
+      }
+    }, 120);
+
+    setInterval(() => {
+      if (visible) {
+        con.className = 'console-underscore hidden';
+        visible = false;
+      } else {
+        con.className = 'console-underscore';
+        visible = true;
+      }
+    }, 400);
+  }
+
+  return (
+    <div className="home">
+      <div className="firstScreen">
+        <div className="firstLeft">
+          <p className="firstLine">Hello Everyone</p>
+          <h1 className="secLine">
+            I’m Kavindu<br />Chathuranga
+          </h1>
+          <div className="console-container">
+            <span id="text"></span>
+            <div className="console-underscore" id="console">
+              &#95;
+            </div>
+          </div>
+          <div className="socialMedia">
+            <img src={github} alt="GitHub" />
+            <img src={behance} alt="Behance" />
+            <img src={linkedin} alt="LinkedIn" />
+          </div>
+          <div className="aboutMe">
+            <button className="aboutMebtn">About Me</button>
+          </div>
+        </div>
+        <div className="firstRight">
+          <img src={Logo} alt="Logo" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
