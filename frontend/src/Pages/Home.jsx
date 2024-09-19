360d854491d53ea360c250d1c531ad19a12df58a
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-09-19 23:53:30 +0530

    new updates on pages

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -354,7 +354,14 @@ const Home = () => {
                 <img src={mongo} alt=' '/>
                 <img src={php} alt=' '/>
                 <img src={mySQL} alt=' '/>
-
+              </div>
+              <div className="techup">
+                <img src={js} alt=' '/>
+                <img src={ts} alt=''/>
+                <imh src={figma1} alt=' '/>
+                <img src={ps} alt=' '/>
+                <img src={pr} alt=' '/>
+                <img src={ae} alt=' '/>
               </div>
           </div>
         </section>
